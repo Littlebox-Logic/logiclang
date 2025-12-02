159a3ce260366e1182d95bcd0e997de3c6928e56
Littlebox-Logic <sunjunpeng2007@126.com>
2025-12-02 23:19:18 +0800

    words define.

--- include/logic/preprocess.h
+++ include/logic/preprocess.h
@@ -11,12 +11,42 @@
 #define MAX_PATH_LENGTH 256
 
 #include <stdio.h>
+#include <stdint.h>
 
-typedef struct _Header
+typedef enum
+{
+	STATIC	, EXTERN	, INTERN	, INLINE	, GLOBAL	, BELONG	, BLOCK		, RKILL		, RLOCK		,
+	REG		, INIT		, THIS		, DEPEND	, VOID		, FLOAT		, LFLOAT	, HFLOAT	, CHAR		,
+	WCHAR	, BYTE		, INT8		, INT16		, INT32		, INT64		, INT128	, UINT8		, UINT16	,
+	UINT32	, UINT64	, UINT128	, UNUM		, BOOL		, ENUM		, DYMLIST	, LINKLS	, DLINKLS	,
+	STRUCT	, UNION		, KW_FILE	, CHANNEL	, STRING	, ARRAY		, SETARR	, CLASS		, OBJECT	,
+	THREAD	, MATRIX	, TENSOR	, VECTOR	, HEADER	, CALL		, FUNC		, RETURN	, PROCESS	,
+	GOTO	, SIZEOF	, LENOF		, TYPEOF	, MALLOC	, DESTROY	, INSTAN	, ASM		, CONVERT	,
+	RENAME	, PUBLIC	, PRIVATE	, PROTECT	, READONLY	, STRICT	, ENVCHK	, CONST		, VAR		,
+	EVAL	, KW_NULL	, KW_EOF	, RESIZE	, IF		, ELSE		, ELIF		, WHILE		, DOWHILE	,
+	FOR		, FORMAP	, BREAK		, CONTINUE	, SWITCH	, CASE		, DEFAULT
+}	Keyword;
+
+typedef enum
+{
+	KEYWORD, INTEGER, NEGAINT, IDENTFR,
+	STRINGV, CHARCTR, PROPRTY, OPERATR,
+	REALNUM, NEGREAL, EOFLINE, EOFFILE,
+	LFTBRAC, RGTBRAC, BLKBEGN, BLKTERM
+}	Wordtype;
+
+typedef struct _Header_Struct
 {
 	char path[MAX_PATH_LENGTH];
 	struct _Header *next;
-}	_header, *header;
+}	_Header, *Header;
+
+typedef struct _Word_Struct
+{
+	void *value;
+	struct _Word_Struct *next;
+	Wordtype type,
+}	_Word, *Word;
 
 FILE *header_inc(const char *);
 FILE *preprocess(const char *);
