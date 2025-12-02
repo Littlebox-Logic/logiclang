50f0b0bd1713135bb4abf46399ad6e99fa619f20
Littlebox-Logic <sunjunpeng2007@126.com>
2025-12-03 00:28:58 +0800

    keywords & operators list, words' lexical struct.

--- include/logic/preprocess.h
+++ include/logic/preprocess.h
@@ -27,6 +27,21 @@ typedef enum
 	FOR		, FORMAP	, BREAK		, CONTINUE	, SWITCH	, CASE		, DEFAULT
 }	Keyword;
 
+const char *keywords = {"static", "extern", "intern" , "inline"	 , "global"	 , "belong" , "block"  , "rkill" , "rlock"  ,
+						"reg"	, "init"  , "this"   , "depend"	 , "void"	 , "float"  , "lfloat" , "hfloat", "char"   ,
+						"wchar"	, "byte"  , "int8"   , "int16"	 , "int32"	 , "int64"  , "int128" , "uint8" , "uint16" ,
+						"uint32", "uint64", "uint128", "unum"	 , "bool"	 , "enum"   , "dymlist", "linkls", "dlinkls",
+						"struct", "union" , "file"   , "channel" , "string"	 , "array"  , "setarr" , "class" , "object" ,
+						"thread", "matrix", "tensor" , "vector"	 , "header"	 , "call"   , "func"   , "return", "process",
+						"goto"	, "sizeof", "lenof"  , "typeof"  , "malloc"	 , "destroy", "instan" , "asm"   , "convert",
+						"rename", "public", "private", "protect" , "readonly", "strict" , "envchk" , "const" , "var"    ,
+						"eval"	, "null"  , "eof"	 , "resize"	 , "if"		 , "else"   , "elif"   , "while" . "dowhile",
+						"for"	, "formap", "break"  , "continue", "switch"	 , "case"	, "default"}
+
+const char *operators = {"+" , "-" , "*", "/", "%", "^", "->", "." ,
+						 "&&", "||", "~", "&", "|", "!", "=" , "==",
+						 "!=", "&" , "?", ":", ">", "<", ">=", "<="}
+
 typedef enum
 {
 	KEYWORD, INTEGER, NEGAINT, IDENTFR,
@@ -35,18 +50,27 @@ typedef enum
 	LFTBRAC, RGTBRAC, BLKBEGN, BLKTERM
 }	Wordtype;
 
-typedef struct _Header_Struct
+typedef struct _Header_Node
 {
 	char path[MAX_PATH_LENGTH];
-	struct _Header *next;
+	struct _Header_Node *next;
 }	_Header, *Header;
 
-typedef struct _Word_Struct
+typedef struct _Word_Node
 {
 	void *value;
-	struct _Word_Struct *next;
-	Wordtype type,
+	struct _Word_Node *next;
+	size_t line;
+	size_t col;
+	Wordtype type;
 }	_Word, *Word;
+
+typedef struct
+{
+	Word	head;
+	Word	tail;
+	size_t	length;
+}	_Wordlist, *Wordlist;
 
 FILE *header_inc(const char *);
 FILE *preprocess(const char *);
